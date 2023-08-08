a3a5d01ebe3ae29139278533e8c175c2dc0d2e41
AnshSachdeva23 <sachdevaansh2002@gmail.com>
2023-08-08 18:27:41 +0530

    next+prev

--- src/Components/News.js
+++ src/Components/News.js
@@ -8,17 +8,49 @@ export class News extends Component {
     super();
     this.state = {
       articles: [],
-      loading: false
+      loading: false,
+      page: 1
     }
   }
 
   async componentDidMount()
   {
-    let url = "https://newsapi.org/v2/everything?q=movies&apiKey=5358deef8dbf46fc884374ca4d034c3f";
+    let url = "https://newsapi.org/v2/everything?q=movies&apiKey=5358deef8dbf46fc884374ca4d034c3f&page=1&pageSize=18";
     let data = await fetch(url);
     let parsedData = await data.json()
     console.log(parsedData);
-    this.setState({articles: parsedData.articles})
+    this.setState({articles: parsedData.articles, totalResults:parsedData.totalResults})
+  }
+
+  handlePrevClick = async() => {
+
+    let url = `https://newsapi.org/v2/everything?q=movies&apiKey=5358deef8dbf46fc884374ca4d034c3f&page=${this.state.page-1}&pageSize=18`;
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
+
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles
+    })
+
+  }
+
+  handleNextClick = async() => {
+    if(this.state.page + 1 > 5) {
+
+    }
+    else {
+      let url = `https://newsapi.org/v2/everything?q=movies&apiKey=5358deef8dbf46fc884374ca4d034c3f&page=${this.state.page+1}&pageSize=18`;
+      let data = await fetch(url);
+      let parsedData = await data.json()
+      console.log(parsedData);
+
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles
+    })
+    }
   }
 
   render() {
@@ -30,9 +62,11 @@ export class News extends Component {
               return <div className="col-md-4" key={element.url}>
               <NewsItem title={element.title.slice(0, 45)} description = {element.description.slice(0, 88)} imageUrl={element.urlToImage} newsUrl={element.url}/>
               </div>  
-
             })}
-               
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page <= 1} type="button" className="btn btn-info" onClick={this.handlePrevClick}>&larr; Previous</button>
+        <button type="button" className="btn btn-info" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
     </div>
     )
