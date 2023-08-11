d97a5902517367746475688a46d11191922ca362
AnshSachdeva23 <sachdevaansh2002@gmail.com>
2023-08-11 19:13:08 +0530

    added categories and react router

--- src/Components/newsItem.js
+++ src/Components/newsItem.js
@@ -8,7 +8,7 @@ export class NewsItem extends Component {
         <div className="card">
         <img src={imageUrl?imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4j0-GbfreurgxtgbySFRLHlKVwVhLhd453g&usqp=CAU"} style={{height: "15em"}} className="card-img-top" alt="..."/>
         <div className="card-body">
-          <h5 className="card-title">{title}...</h5>
+          <h5 className="card-title" style={{fontWeight: 'bold'}}>{title}</h5>
           <p className="card-text">{description}...</p>
           <a href={newsUrl} target="_blank" rel="noreferrer" className="w3-button w3-pink">Read More</a>
         </div>
